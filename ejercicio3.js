5a486db0a747039c682f4e055c2d3d0ca205b66f
Ignacio <naxete7@hotmail.es>
2022-10-04 10:02:16 +0200

    enunciado

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,3 +1,6 @@
+//3. Modifica la aplicación anterior, para que nos pida el nombre que queremos
+//introducir(recuerda usar prompt).
+
 var nombre = prompt("Escriba su nombre");
 
 console.log("Bienvenido " + nombre);
